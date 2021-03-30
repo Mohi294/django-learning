b0b6184998dacc0e9cb4dd36d735225033c06bcd
sosamo <sosamo@sosamo.s>
2021-03-31 00:04:17 +0430

    error at post-update

--- blog1/views.py
+++ blog1/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 
 # posts = [
 #     {
@@ -33,6 +37,15 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'blog1/about.html', {'title': 'About'})
